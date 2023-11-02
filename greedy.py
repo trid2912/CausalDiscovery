d09544c2a70725c992b01343f70b1799098f040f
trid4012 <trid4012@gmail.com>
2023-11-02 22:15:39 +0700

    fix greedy.py

--- greedy.py
+++ greedy.py
@@ -9,6 +9,7 @@ base_estimate = BaseEstimator(data)
 
 nodes = list(base_estimate.state_names.keys())
 num_nodes = len(nodes)
+print(nodes)
 
 adj_matrix = np.zeros((num_nodes, num_nodes))
 best_score = -999999
@@ -23,8 +24,7 @@ while True:
                 for k in range(num_nodes):
                     parents = np.nonzero(copy_mat[:,k])[0]
                     parents = [nodes[k] for k in parents]
-                    
-                bic_score += base_estimate.local_score(nodes[k], parents)
+                    bic_score += base_estimate.local_score(nodes[k], parents)
                 if bic_score > best_score:
                     next_state = copy_mat
                     best_score = bic_score
@@ -35,3 +35,21 @@ while True:
     if np.array_equal(next_state, adj_matrix):
         break
     adj_matrix = next_state
+
+true_matrix = np.zeros((num_nodes, num_nodes))
+true_matrix[0,2] = 1
+true_matrix[1,3] = 1
+true_matrix[1,4] = 1
+true_matrix[2,5] = 1
+true_matrix[3,5] = 1
+true_matrix[4,7] = 1
+true_matrix[5,6] = 1
+true_matrix[5,7] = 1
+print(is_dag(true_matrix))
+bic_score = 0
+for k in range(num_nodes):
+    parents = np.nonzero(true_matrix[:,k])[0]
+    parents = [nodes[k] for k in parents]
+    bic_score += base_estimate.local_score(nodes[k], parents)
+print(true_matrix)
+print(bic_score)
